b2b512a1eb69c0a9655bc2908450e3970a6b422f
alissomberg <alissomberg797@gmail.com>
2026-01-12 20:06:33 -0300

    Exercícios da 2º lista

---
+++ Exercícios sobre Estruturas de controle/ex1.js
@@ -0,0 +1,10 @@
+/*Atividade 1 – Contagem Simples
+Nível: Iniciante
+Crie um programa em JavaScript que:
+Utilize um laço de repetição
+Conte de 1 até 10
+Exiba cada número no console*/
+
+for(let i = 1;i<=10;i++){
+    console.log(i)
+}
---
+++ Exercícios sobre Estruturas de controle/ex4.js
@@ -0,0 +1,14 @@
+/*Atividade 4 – Contagem Regressiva
+Nível: Intermediário
+Crie um programa que:
+Utilize um laço de repetição
+Conte de 20 até 1
+Exiba cada número no console
+Quando o número for igual a 10, exiba a mensagem:
+Metade do caminho!*/
+
+for(let i = 20;i>=1;i--){
+    if(i == 10){console.log(`${i} Metade do caminho!`)}
+    else(console.log(i))
+}
+
